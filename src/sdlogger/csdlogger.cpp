cd4ca602c7253709918c383106c5928dddd3e44a
cuihairu <chuihairu@gmail.com>
2025-10-27 23:58:03 +0800

    feat(sdlogger): implement CSDLogger methods; test: add delegation tests and make file logger smoke test platform-agnostic

---
+++ src/sdlogger/csdlogger.cpp
@@ -0,0 +1,60 @@
+#include "sdlogger.h"
+#include <cstdarg>
+#include <cstdio>
+#include <string>
+
+namespace SSCP {
+
+CSDLogger::CSDLogger()
+  : m_pLogger(nullptr)
+{
+    m_aszLogPrefix[0] = ""; // unused
+    m_aszLogPrefix[1] = "[D] ";
+    m_aszLogPrefix[2] = "[I] ";
+    m_aszLogPrefix[3] = "[W] ";
+    m_aszLogPrefix[4] = "[C] ";
+}
+
+CSDLogger::~CSDLogger() {}
+
+void CSDLogger::SetLogger(ISSLogger* pLogger) {
+    m_pLogger = pLogger;
+}
+
+void CSDLogger::Critical(const char *format, ...) {
+    va_list ap; va_start(ap, format); _Log(LOGLV_CRITICAL, format, ap); va_end(ap);
+}
+void CSDLogger::Info(const char *format, ...) {
+    va_list ap; va_start(ap, format); _Log(LOGLV_INFO, format, ap); va_end(ap);
+}
+void CSDLogger::Warn(const char *format, ...) {
+    va_list ap; va_start(ap, format); _Log(LOGLV_WARN, format, ap); va_end(ap);
+}
+void CSDLogger::Debug(const char *format, ...) {
+    va_list ap; va_start(ap, format); _Log(LOGLV_DEBUG, format, ap); va_end(ap);
+}
+
+void CSDLogger::_Log(UINT32 dwLevel, const char *pszFormat, va_list argptr)
+{
+    if (!m_pLogger || !pszFormat) return;
+
+    char buf[2048];
+#ifdef _MSC_VER
+    vsnprintf_s(buf, sizeof(buf), _TRUNCATE, pszFormat, argptr);
+#else
+    vsnprintf(buf, sizeof(buf), pszFormat, argptr);
+#endif
+
+    const char* prefix = "";
+    if (dwLevel & LOGLV_CRITICAL) prefix = m_aszLogPrefix[4];
+    else if (dwLevel & LOGLV_WARN) prefix = m_aszLogPrefix[3];
+    else if (dwLevel & LOGLV_INFO) prefix = m_aszLogPrefix[2];
+    else if (dwLevel & LOGLV_DEBUG) prefix = m_aszLogPrefix[1];
+
+    std::string line(prefix);
+    line += buf;
+    m_pLogger->LogText(line.c_str());
+}
+
+} // namespace SSCP
+
--- tests/test_sdlogger.cpp
+++ tests/test_sdlogger.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "sdlogger.h"
+#include <string>
 
 using namespace SSCP;
 
@@ -11,15 +12,32 @@ TEST(sdlogger, file_logger_factory_not_null) {
 
 TEST(sdlogger, file_logger_basic_init_and_log) {
     auto* p = SDCreateFileLogger(&SDLOGGER_VERSION);
-#if defined(_WIN32)
     ASSERT_TRUE(p->Init(".", "sse_log", LOG_MODE_SINGLE_FILE));
     EXPECT_TRUE(p->LogText("hello"));
     UINT8 buf[4] = {1,2,3,4};
     EXPECT_TRUE(p->LogBinary(buf, 4));
-#else
-    // non-Windows: stub implementation for now
-    ASSERT_TRUE(p->Init(".", "sse_log", LOG_MODE_SINGLE_FILE));
-    EXPECT_TRUE(p->LogText("hello"));
-#endif
     p->Release();
+}
+
+namespace {
+class TestLogger : public ISSLogger {
+public:
+    bool SSAPI LogText(const char* pszLog) override {
+        last = pszLog ? pszLog : ""; return true;
+    }
+    bool SSAPI LogBinary(const UINT8* pLog, UINT32 dwLen) override {
+        (void)pLog; (void)dwLen; return true;
+    }
+    std::string last;
+};
+}
+
+TEST(sdlogger, csdlogger_delegates_to_logger) {
+    TestLogger tl;
+    CSDLogger clogger;
+    clogger.SetLogger(&tl);
+    clogger.Info("hello %d", 42);
+    EXPECT_NE(tl.last.find("hello 42"), std::string::npos);
+    clogger.Warn("warn");
+    EXPECT_NE(tl.last.find("warn"), std::string::npos);
 }
